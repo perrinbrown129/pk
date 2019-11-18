e2259d871ee21203d3ec5bfb1c3d0b9b5be4f2d1
Perrin Brown <macadmin@Perrins-Macbook-Pro.local>
2019-11-18 17:40:35 -0500

    day one DONE

--- bookmarks_api/models/bookmarks.js
+++ bookmarks_api/models/bookmarks.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const bookmarkSchema = mongoose.Schema({
-    title: String,
-    url: String
-})
+  name: String,
+  url: String
+});
 
-module.exports = mongoose.model('Bookmark', bookmarkSchema)
+module.exports = mongoose.model("Bookmark", bookmarkSchema);
